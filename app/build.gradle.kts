af871b397f978210bf87a7c27e75b7e35a20e296
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-04-19 16:43:56 -0400

    Updated dependencies
    - Removed Guava as it was never used
    - Added Moshi JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,46 +7,47 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+	// Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+	id("org.jetbrains.kotlin.jvm") version "1.5.31"
 
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+	// Apply the application plugin to add support for building a CLI application in Java.
+	application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+	// Use Maven Central for resolving dependencies.
+	mavenCentral()
 }
 
 dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+	// Align versions of all Kotlin components
+	implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// Use the Kotlin JDK 8 standard library.
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:30.1.1-jre")
+	// Moshi JSON
+	implementation("com.squareup.moshi:moshi:1.13.0")
+	implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+	// Use the Kotlin test library.
+	testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+	// Use the Kotlin JUnit integration.
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
-    // Define the main class for the application.
-    mainClass.set("app.shamilton.adhdtimcard.AppKt")
+	// Define the main class for the application.
+	mainClass.set("app.shamilton.adhdtimecard.AppKt")
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(11))
+	toolchain.languageVersion.set(JavaLanguageVersion.of(11))
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+	kotlinOptions {
+		jvmTarget = "11"
+	}
 }
