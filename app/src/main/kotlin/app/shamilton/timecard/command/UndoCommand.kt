cf68d5553ab99217a700cf55346e5884c6a3ebed
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-05-10 17:30:11 -0400

    Implemented UndoCommand

--- app/src/main/kotlin/app/shamilton/timecard/command/UndoCommand.kt
+++ app/src/main/kotlin/app/shamilton/timecard/command/UndoCommand.kt
@@ -1,11 +1,41 @@
 package app.shamilton.timecard.command
 
+import app.shamilton.timecard.core.TimeEntries
+import app.shamilton.timecard.core.TimeEntry
+import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.terminal.Terminal
+
 class UndoCommand : ICommand {
 
 	override val m_Name: String = "UNDO"
 
+	private val _t = Terminal()
+
 	override fun execute() {
-		TODO("Not yet implemented")
+		val timeEntries = TimeEntries.loadFromFile()
+		if (timeEntries.m_Entries.isEmpty()){
+			_t.println(yellow("Nothing to undo!"))
+			println("Clock in first using 'timecard in'.")
+			return
+		}
+
+		if(timeEntries.isClockedIn()){
+			val removedEntry: TimeEntry = timeEntries.m_Entries.removeLast()
+			_t.println("Undo: clock ${green("in")} at ${green(removedEntry.startTime.toString())}")
+			_t.println(red("Currently clocked out"))
+
+			if(timeEntries.m_Entries.isEmpty())
+				timeEntries.deleteFile()
+			else
+				timeEntries.saveToFile()
+		} else {
+			val lastEntry: TimeEntry = timeEntries.m_Entries.last()
+			_t.println("Undo: clock ${red("out")} at ${red(lastEntry.endTime.toString())}")
+			_t.println(green("Currently clocked in"))
+
+			lastEntry.endTime = null
+			timeEntries.saveToFile()
+		}
 	}
 
 }
