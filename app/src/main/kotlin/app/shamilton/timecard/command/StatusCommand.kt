a801400cc9ecbe610e85d57726842b052fcf19bd
Stephen C. Hamilton <hamilton.stephen@vast-inc.com>
2022-05-11 12:59:32 -0400

    Worked time status implementation

--- app/src/main/kotlin/app/shamilton/timecard/command/StatusCommand.kt
+++ app/src/main/kotlin/app/shamilton/timecard/command/StatusCommand.kt
@@ -3,6 +3,9 @@ package app.shamilton.timecard.command
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.terminal.Terminal
 import app.shamilton.timecard.core.TimeEntries
+import app.shamilton.timecard.core.TimeEntry
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 
 class StatusCommand : ICommand {
 
@@ -11,12 +14,44 @@ class StatusCommand : ICommand {
 
     private val _t = Terminal()
 
+    private fun getTimeWorked(timeEntries: TimeEntries): LocalTime {
+        var timeWorked = LocalTime.of(0, 0)
+        for (entry: TimeEntry in timeEntries.m_Entries) {
+            val startTime: LocalTime = entry.startTime
+            val endTime: LocalTime = entry.endTime ?: LocalTime.now().truncatedTo(ChronoUnit.MINUTES)
+
+            val startTimeMinutes: Int = startTime.hour * 60 + startTime.minute
+            val difference: LocalTime = endTime.minusMinutes(startTimeMinutes.toLong())
+            val differenceMinutes: Int = difference.hour * 60 + difference.minute
+
+            timeWorked = timeWorked.plusMinutes(differenceMinutes.toLong())
+        }
+
+        return timeWorked
+    }
+
     override fun execute() {
         val timeEntries = TimeEntries.loadFromFile()
-        val clockState = if(timeEntries.isClockedIn()) green("in") else red("out")
-        _t.println("Currently clocked $clockState.")
+        if(timeEntries.m_Entries.isEmpty()) {
+            _t.println(yellow("You haven't clocked in yet today! Use 'timecard in' to clock in."))
+            return
+        }
 
-        // TODO: Print how long worked
+        val clockState = if(timeEntries.isClockedIn()) "in" else "out"
+        val color = if(timeEntries.isClockedIn()) green else red
+        _t.println(color("Currently clocked $clockState."))
+
+
+        // Print how long worked
+        val timeWorked: LocalTime = getTimeWorked(timeEntries)
+        val minutes: String = if(timeWorked.minute == 1) "minute" else "minutes"
+        if(timeWorked.hour > 0){
+            val hours: String = if(timeWorked.hour == 1) "hour" else "hours"
+            println("Worked for ${timeWorked.hour} $hours and ${timeWorked.minute} $minutes")
+        } else {
+            println("Worked for ${timeWorked.minute} $minutes")
+        }
+
         // TODO: Print how many hours to the nearest quarter
     }
 
