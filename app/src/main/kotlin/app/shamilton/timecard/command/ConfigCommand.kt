c312e90afc24de3446821c9d895bf26d14331d08
Stephen C. Hamilton <hamilton.stephen@vast-inc.com>
2022-05-24 10:01:04 -0400

    Hacky implementation of ConfigCommand

--- app/src/main/kotlin/app/shamilton/timecard/command/ConfigCommand.kt
+++ app/src/main/kotlin/app/shamilton/timecard/command/ConfigCommand.kt
@@ -1,5 +1,14 @@
 package app.shamilton.timecard.command
 
+import app.shamilton.timecard.App
+import app.shamilton.timecard.config.CleanInterval
+import app.shamilton.timecard.config.Configuration
+import com.github.ajalt.mordant.rendering.TextColors.yellow
+import com.github.ajalt.mordant.rendering.TextColors.brightMagenta
+import com.github.ajalt.mordant.rendering.TextColors.green
+
+import com.github.ajalt.mordant.terminal.Terminal
+
 class ConfigCommand : ICommand {
 
 	override val m_Name: String = "CONFIG"
@@ -7,8 +16,43 @@ class ConfigCommand : ICommand {
 	override val m_DetailedHelp: String? = null //TODO: List configurations
 	override val m_HelpArgs: List<String> = listOf() // TODO
 
+	private val _t = Terminal()
+
 	override fun execute() {
-		TODO("Not yet implemented")
+		val configName = App.getArg(1)
+		val configValue = App.getArg(2)
+
+		if(configName == null){
+			_t.println(yellow("Invalid number of arguments. See 'timecard help config' for usage."))
+			return
+		}
+
+		val config = Configuration.loadFromFile()
+
+		// TODO: Hmm... I should make configuration classes
+		when(configName) {
+			"CLEAN_INTERVAL" -> {
+				if(configValue == null){
+					// display value
+					_t.println("clean_interval is currently set to ${brightMagenta(config.clean_interval.toString())}")
+				} else {
+					// set value
+					val cleanInterval: CleanInterval? = CleanInterval.get(configValue.trim())
+					if (cleanInterval == null) {
+						_t.println(yellow("Unknown value for clean_interval. Known values are ${brightMagenta("manually")}${yellow(",")} ${brightMagenta("daily")}${yellow(",")} ${brightMagenta("weekly")}${yellow(",")} and ${brightMagenta("monthly")}${yellow(".")}"))
+						return
+					}
+					config.clean_interval = cleanInterval
+					_t.println("Set ${green(configName.lowercase())} to ${green(configValue)}")
+				}
+			}
+			else -> {
+				_t.println(yellow("Unknown configuration! See 'timecard help config' for list of configs."))
+				return
+			}
+		}
+
+		config.saveToFile()
 	}
 
 }
--- app/src/main/kotlin/app/shamilton/timecard/config/CleanInterval.kt
+++ app/src/main/kotlin/app/shamilton/timecard/config/CleanInterval.kt
@@ -1,8 +1,19 @@
 package app.shamilton.timecard.config
 
-enum class CleanInterval(val days: Int, val automatic: Boolean = true) {
-	MANUALLY(1, false),
+enum class CleanInterval(val days: Int) {
+	MANUALLY(1),
 	DAILY(1),
 	WEEKLY(7),
-	MONTHLY(30),
+	MONTHLY(30);
+
+	companion object {
+		@JvmStatic fun get(name: String?): CleanInterval? {
+			if (name == null) return null
+			return try {
+				CleanInterval.valueOf(name.uppercase())
+			} catch (e: IllegalArgumentException) {
+				null
+			}
+		}
+	}
 }
