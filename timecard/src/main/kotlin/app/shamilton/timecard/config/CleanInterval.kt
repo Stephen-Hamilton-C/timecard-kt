aa6107ca15f64eb3f427f5318adb34321c7a1b15
Stephen C. Hamilton <hamilton.stephen@vast-inc.com>
2022-05-27 16:20:42 -0400

    Moved CleanInterval enum into CleanIntervalConfig

--- timecard/src/main/kotlin/app/shamilton/timecard/config/CleanInterval.kt
+++
@@ -1,8 +0,0 @@
-package app.shamilton.timecard.config
-
-enum class CleanInterval(val days: Int) {
-	MANUALLY(1),
-	DAILY(1),
-	WEEKLY(7),
-	MONTHLY(30),
-}
--- timecard/src/main/kotlin/app/shamilton/timecard/config/CleanIntervalConfig.kt
+++ timecard/src/main/kotlin/app/shamilton/timecard/config/CleanIntervalConfig.kt
@@ -1,5 +1,12 @@
 package app.shamilton.timecard.config
 
+enum class CleanInterval(val days: Int) {
+	MANUALLY(1),
+	DAILY(1),
+	WEEKLY(7),
+	MONTHLY(30),
+}
+
 class CleanIntervalConfig : Config() {
 
 	override val m_Name: String = "CLEAN_INTERVAL"
