159ed633a0ad1e140c5b3ea2c105e031c8df9965
Stephen C. Hamilton <hamilton.stephen@vast-inc.com>
2022-07-07 09:22:09 -0400

    Improved feedback for ConfigCommand errors

--- src/main/kotlin/app/shamilton/timecardkt/command/ConfigCommand.kt
+++ src/main/kotlin/app/shamilton/timecardkt/command/ConfigCommand.kt
@@ -3,6 +3,7 @@ package app.shamilton.timecardkt.command
 import app.shamilton.timecardkt.App
 import app.shamilton.timecardkt.Color.yellow
 import app.shamilton.timecardkt.Color.MAGENTA
+import app.shamilton.timecardkt.Color.RESET
 import app.shamilton.timecardkt.Color.YELLOW
 import app.shamilton.timecardkt.Color.magenta
 import app.shamilton.timecardkt.config.Config
@@ -18,37 +19,43 @@ class ConfigCommand : ICommand {
 			for ((i: Int, config: Config) in ConfigList.CONFIGS.withIndex()) {
 				val tab = if(i == 0) "" else "	"
 				detailedHelp += "$tab- ${config.m_Name}: ${config.m_Help}\n"
-				detailedHelp += "		Possible values: ${config.m_PossibleValues.joinToString(", ")}\n"
+				detailedHelp += "		Possible values: ${getColorfulValues(config)}\n"
 			}
 			return detailedHelp
 		}
 	override val m_HelpArgs: List<String> = listOf("<name>", "[value]")
 
+	private fun getColorfulValues(config: Config, originalColor: String = RESET): String = "$MAGENTA${config.m_PossibleValues.joinToString("$originalColor, $MAGENTA")}$originalColor.$RESET"
+
 	override fun execute() {
 		val configName = App.getArg(1)
-		val configValue = App.getArg(2)
+		val newConfigValue = App.getArg(2)
 
 		if(configName == null){
-			println(yellow("Invalid number of arguments. See '${App.NAME} help config' for usage."))
+			println("Full list of configurations:")
+			println("	$m_DetailedHelp")
 			return
 		}
 
 		val foundConfig: Config? = ConfigList.CONFIGS.find { it.m_Name == configName }
 		if(foundConfig == null) {
-			println(yellow("Unknown configuration! See '${App.NAME} help config' for list of configs."))
+			println(yellow("Unknown configuration! Here's the full list of configurations:"))
+			println("	$m_DetailedHelp")
 			return
 		}
 
-		if(configValue == null){
+		if(newConfigValue == null){
 			// Display value
 			println("${foundConfig.m_Name.lowercase()} is currently set to ${magenta(foundConfig.m_Value)}")
-		} else if(foundConfig.m_PossibleValues.contains(configValue)) {
+			println(foundConfig.m_Help)
+			println("Possible values: ${getColorfulValues(foundConfig)}")
+		} else if(foundConfig.m_PossibleValues.contains(newConfigValue)) {
 			// Set value
-			foundConfig.m_Value = configValue
-			println("Set ${foundConfig.m_Name.lowercase()} to ${magenta(configValue)}")
+			foundConfig.m_Value = newConfigValue
+			println("Set ${foundConfig.m_Name.lowercase()} to ${magenta(newConfigValue)}")
 		} else {
 			// Invalid value
-			println(yellow("Unknown value for ${foundConfig.m_Name.lowercase()}. Known values are $MAGENTA${foundConfig.m_PossibleValues.joinToString("$YELLOW, $MAGENTA")}${yellow(".")}"))
+			println(yellow("Unknown value for ${foundConfig.m_Name.lowercase()}. Known values are ${getColorfulValues(foundConfig, YELLOW)}"))
 		}
 	}
 
